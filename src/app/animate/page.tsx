bfbef5b229cada7e1c93b9d2a904827f4061befc
Josh Oladunni <josholadunni@gmail.com>
2025-04-24 21:11:12 +0100

    Created layout for Animate.tsx

---
+++ src/app/animate/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { animate } from "animejs";
+
+export default function Animate() {
+  return (
+    <div className="w-full">
+      <div className="anim-box bg-red-400 w-fit px-5 mx-auto mt-20">
+        Animate
+      </div>
+      ;
+    </div>
+  );
+}
