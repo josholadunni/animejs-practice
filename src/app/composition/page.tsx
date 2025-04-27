30c8ab7bd60e9a723505cfbf92347447299a858c
Josh Oladunni <josholadunni@gmail.com>
2025-04-27 18:48:53 +0100

    Switched querySelectorAll with useRef array

---
+++ src/app/composition/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Composition() {
+  return (
+    <div className="w-full">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+  );
+}
